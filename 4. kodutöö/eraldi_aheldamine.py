b3049bf9d261f892eeca3b0914f641aab6d7380d
irispajus <124361387+irispajus@users.noreply.github.com>
2023-12-05 19:59:41 +0200

    Update eraldi_aheldamine.py

--- 4. kodutöö/eraldi_aheldamine.py
+++ 4. kodutöö/eraldi_aheldamine.py
@@ -5,34 +5,35 @@
 # lisama ühele indeksile mitut võtit, mis saavad räsimise 
 # tulemusel sama indeksi
 class lingitud_list:
-    def __init__ (self, key, value):
-        self.key = key 
-        self.value = value 
+    def __init__ (self, voti, rasi_vaartus):
+        self.voti = voti 
+        self.rasi_vaartus = rasi_vaartus 
         self.next = None 
 
 # loon klassiga tühja massiivi
 class eraldi_aheldamisega_räsitabel:
-    def __init__(self, size):
-        self.size = size
-        self.table = [None] * size
+    def __init__(self, suurus):
+        self.suurus = suurus
+        self.table = [None] * suurus
+        
 
 # kasutan tähtede ASCII koodide summasid räsimiseks ja teen tehte
 # ASCII koodi summa/räsitabel ja leian jäägi, jäägist saab sõnale indeks
-    def räsifunktsioon(self, key):
-        return sum(ord(char) for char in key) % self.size
+    def räsifunktsioon(self, voti):
+        return sum(ord(char) for char in voti) % self.suurus
     
 # lisan tabelisse saadud indeksi
-    def insert(self, key, value):
-        index = self.räsifunktsioon(key)
+    def insert(self, voti, rasi_vaartus):
+        index = self.räsifunktsioon(voti)
 
 # sõna pannakse eadli ahelasse saadud indeksil        
         if self.table[index] is None:
-            self.table[index] = lingitud_list(key, value)
+            self.table[index] = lingitud_list(voti, rasi_vaartus)
 
 
 # see osa on massivist otsimiseks, esimesena leitakse sõna indeks
-    def get(self, key):
-        index = self.räsifunktsioon(key) 
+    def get(self, voti):
+        index = self.räsifunktsioon(voti) 
 
 # siin osas lisan ahela kui seda juba pole ja vaatan saadud indeksi
 # räsi väärtuseid, mis võisid saada salvestatud juba eraldi ahelana
@@ -40,8 +41,8 @@ class eraldi_aheldamisega_räsitabel:
 # ahelas leitud võti + räsi väärtuse paarile
         current = self.table[index]
         while current is not None:
-            if current.key == key:
-                return current.value
+            if current.voti == voti:
+                return current.rasi_vaartus 
             current = current.next
 
 # seda osa saab kasutada selleks et kontrollida mis juhtub siis kui
@@ -49,7 +50,7 @@ class eraldi_aheldamisega_räsitabel:
 
 # siin vaatan saadud indeksi räsi väärtuseid, leitud võti + räsi 
 # väärtuse paari, kui 41- 45 real koodi mitte kasutada siis saab näidata 
-# kuidas ilma lingitud listita tekkib kollisioon
+# kuidas ilma lingitud listita tekkib kokkupõrge
         # for k, v in self.table[index]:
         #     if k == key:
         #         return v
@@ -58,7 +59,7 @@ class eraldi_aheldamisega_räsitabel:
         return None
 
 # siin osas on andmed, millega koodi tööd testida
-räsitabel = eraldi_aheldamisega_räsitabel(size = 20)
+räsitabel = eraldi_aheldamisega_räsitabel(suurus = 20)
 
 räsitabel.insert("John Doe", 19)
 räsitabel.insert("Jane Doe", 19) # siin ei ole jääk 19, vaid kasutasin lihtsalt testimiseks
